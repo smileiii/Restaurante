b91baa6eec9ae992be1f8659a9ead1065b60f5bd
Vina del Mar <vina@duoc.acad>
2019-10-16 11:10:15 -0300

    subir a git la pagina

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+
+let btnmenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnmenu.addEventListener('click',function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
